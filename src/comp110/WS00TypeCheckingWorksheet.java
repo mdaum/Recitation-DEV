437d4292ddced693b806a2b9785448356cace210
Kris Jordan <krisjordan@gmail.com>
2016-01-31 18:04:54 -0500

    Intermediate commit working toward recitation 01.

---
+++ src/comp110/WS00TypeCheckingWorksheet.java
@@ -0,0 +1,199 @@
+package comp110;
+
+/**
+ * Worksheet 01 - Parameter, Argument, and Return Types
+ * 
+ * In this recitation we will explore the importance of using the correct types
+ * when calling methods.
+ */
+
+class WS00TypeCheckingWorksheet {
+
+  /*
+   * GUIDED SECTION
+   * 
+   * We will work through these problems together in recitation.
+   */
+
+  /**
+   * Problem 1
+   * 
+   * A B C / It's as easy as, 1 2 3 / As simple as do re me / A B C, 1 2 3 /
+   * Baby, you and me!
+   */
+  int p01TheJackson() {
+    return 0;
+  }
+
+  /**
+   * Problem 2
+   * 
+   * Go, go, go, go / go, go, go shorty / It's your birthday / We gon' party
+   * like it's yo birthday
+   */
+  double p02ARapperConvertedToDollars() {
+    return 0.0;
+  }
+
+  /**
+   * Problem 3
+   * 
+   * Give me a break / give me a break / break me off a piece of that ........
+   * ___ ___ ___
+   * 
+   * https://www.youtube.com/watch?v=F5GXUs_-YTg
+   */
+  String p03WhatAndyCouldntRemember() {
+    return "<answer>";
+  }
+
+  /**
+   * Problem 4
+   * 
+   * The Duke game is in two weeks. Given UNC's score as a parameter, calculate
+   * Duke's score.
+   * 
+   * Robertson's from Duke... love you all! -Kris
+   */
+  int p04DukeScoreOnFebruary17th(int uncScoreOnFebruary17th) {
+    return 2147483647;
+  }
+
+  /**
+   * Problem 5
+   * 
+   * Recall a square from Geometry and now imagine we have a class defined to
+   * model a Square.
+   * 
+   * The Square class contains the following method:
+   * 
+   * double getWidth() - returns the width of the Square instance
+   * 
+   * The method you are to write is given an instance of Square named aSquare.
+   * 
+   * How would you call the getWidth method on aSquare in order to calculate the
+   * area aSquare?
+   */
+  double p05TheAreaOfASquare(Square aSquare) {
+    return 0;
+  }
+
+  /**
+   * Problem 6
+   * 
+   * An instance named clues of the ClueGiver class is given to the following
+   * method as a parameter.
+   * 
+   * The ClueGiver class contains the following method:
+   * 
+   * String giveMeOne(int problemNumber) - gives a random clue for problemNumber
+   * 
+   * In this case, you must ask clues for clues and print them to your console
+   * using the System.out.println(<value>) method discussed in the
+   * "Parameters and Arguments" lecture slide deck. Here's an example usage:
+   * 
+   * System.out.println(clues.giveMeOne(1));
+   */
+  String p06MajorKeyToSuccessInCOMP110(WS00ClueGiver clues) {
+    return "<answer>";
+  }
+
+  /*
+   * TEAMWORK SECTION
+   * 
+   * Work on the following problems with your partner or small group.
+   */
+
+  /**
+   * Problem 7
+   * 
+   * What is your partner's first name?
+   */
+  String p07MyPartnersFirstName() {
+    return "";
+  }
+
+  /**
+   * Problem 8
+   * 
+   * How many years has your partner been at UNC? Round up!
+   */
+  int p08YearsAtUNC() {
+    return 0;
+  }
+
+  /**
+   * Problem 9
+   * 
+   * Think of an *ODD* number between 1 and 99, inclusive. Have your partner
+   * guess your number. After each guess, respond (correctly! no tricks) with
+   * "higher" or "lower". See who can guess whose in fewer tries.
+   * 
+   * When you're done, fill in the following return value with your partner's
+   * number divided by 2.0.
+   */
+  double p09AnOddBinarySearch() {
+    return 0.0;
+  }
+
+  /**
+   * Problem 10
+   * 
+   * Given a year born, e.g. 1985, calculate the age of someone born in that
+   * year on January 1st.
+   */
+  int p10CalculateAge(int yearBorn) {
+    return 0;
+  }
+
+  /**
+   * Problem 11
+   * 
+   * Given an age, calculate the year of someone born, assuming a born on date
+   * of January 1st.
+   */
+  int p11YearBorn(int age) {
+    return 0;
+  }
+
+  /**
+   * Problem 12
+   * 
+   * Given an instance of a Square, just like in problem #5, calculate its
+   * perimeter.
+   */
+  double p12ThePerimeterOfASquare(Square aSquare) {
+    return 0;
+  }
+
+  /**
+   * Problem 13
+   * 
+   * Given a ClueGiver class, just like in Problem # 6, solve the puzzle by
+   * returning another major key to success in COMP110.
+   * 
+   * Remember:
+   * 
+   * System.out.println(<value>) can print values for you in your running code!
+   */
+  String p13AnotherMajorKeyToSuccessInCOMP110(WS00ClueGiver clues) {
+    return "<answer>";
+  }
+
+  /*
+   * CHALLENGE PROBLEMS -- NOT FOR CREDIT, JUST FOR STREET CRED
+   * ----------------------------------------------------------
+   * 
+   * Go ahead and submit your worksheet to COMP110.com BEFORE starting these to
+   * ensure you receive credit for your recitation work!
+   * 
+   * The following problems are just for fun *IF* you have time to complete
+   * them. You can resubmit solutions for these challenge problems up to 24
+   * hours following your recitation section.
+   */
+
+  /**
+   * We'll start challenge problems next week!
+   */
+
+}
---
+++ src/comp110/WS00TypeCheckingWorksheetTester.java
@@ -0,0 +1,9 @@
+package comp110;
+
+public class WS00TypeCheckingWorksheetTester extends WS00TestsGUI {
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+}
