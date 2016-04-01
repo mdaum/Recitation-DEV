fd89709ecbb495f7387938f3628e8ae311d5e843
Sarah White <sarahw14@live.unc.edu>
2016-04-01 16:32:00 -0400

    Started the worksheet for second midterm review.

---
+++ src/comp110/WS04SecMidtermReview.java
@@ -0,0 +1,93 @@
+package comp110;
+
+import java.util.ArrayList;
+
+public class WS04SecMidtermReview {
+	/*
+	 * GUIDED SECTION
+	 * 
+	 * We will work through these problems together in recitation.
+	 */
+	
+	/**
+	 * Problem 1
+	 * 
+	 * Let's implement the code to traverse through the given ArrayList of Rectangles and
+	 * return the sum of the area of all the Rectangles. Use a for loop. Then redo with a 
+	 * for-each loop!
+	 * 
+	 * 
+	 */
+	int p01AreaSum() {
+		//ArrayList<Shapes> ourRects = ?
+		
+		return 0;
+	}
+	
+	/**
+	 * Problem 2
+	 * 
+	 * Implement the factorial method. Remember that 4! = 4*3*2*1. Use a for loop.
+	 * 
+	 */
+	int p02Factorial(int n) {
+		
+		return 0;
+	}
+	
+	/**
+	 * Problem 3
+	 * 
+	 * Given an ArrayList of Players, finish the method that creates a new ArrayList of Players
+	 * that is reversed from the original. Use a for loop.
+	 * 
+	 */
+	int p03Reverse() {
+		//ArrayList<Player> ourPlayers = ?
+		return 0;
+	}
+	
+
+
+	/*
+	 * TEAMWORK SECTION
+	 * 
+	 * Work on the following problems with your partner or small group. Note
+	 * that everyone must submit their own worksheet.
+	 */
+	
+	/**
+	 * Problem 4
+	 * 
+	 * This is a multiple step question! We are going to reverse an ArrayList using a different 
+	 * strategy. 
+	 * 
+	 * First create a swap method. This method should be private and called p04swap and should 
+	 * take two int inputs and an ArrayList. The method should swap the elements at indexes i and j. 
+	 * 
+	 * Secondly, finish the p04Reverse method. This method should use our p04swap method to 
+	 * reverse the elements in the given ArrayList. Use a for loop. Note the helper method.
+	 * 
+	 */
+	
+	int p04Reverse() {
+		//ArrayList<Player> ourPlayers = ?
+		return 0;
+	}
+	
+	int 0
+	
+	
+	/**
+	 * Problem 5
+	 * 
+	 * 
+	 * 
+	 */
+	
+	int p04Reverse() {
+		//ArrayList<Player> ourPlayers = ?
+		return 0;
+	}
+	
+}
